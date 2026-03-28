7cd45ef65de63c239140b1a2eb98fc277da7bd98
HST159075 <alamtasin18@gmail.com>
2026-03-28 23:27:51 +0600

    modified:   src/app.ts

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ import { auth } from "./lib/auth.js";
 const app: Application = express();
 
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: "https://tube-server.onrender.com",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "Cookie"],

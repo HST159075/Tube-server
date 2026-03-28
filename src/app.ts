04aa84a0bd1b4ab7e7836535f27ae3ba2dacce18
HST159075 <alamtasin18@gmail.com>
2026-03-29 00:05:20 +0600

    fix : app.ts

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ import { auth } from "./lib/auth.js";
 const app: Application = express();
 
 app.use(cors({
-    origin: "https://tube-server.onrender.com",
+    origin: "https://chine-tube.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "Cookie"],

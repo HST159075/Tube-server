cbf3b26f4bd4b9051392f74b9992adb96e4235f7
HST159075 <alamtasin18@gmail.com>
2026-04-02 17:12:56 +0600

    fix: add appUrl for google oauth redirect

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -7,6 +7,7 @@ export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
+  appUrl: process.env.CLIENT_URL,
   emailAndPassword: {
     enabled: true,
   },

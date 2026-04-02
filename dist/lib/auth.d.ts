3c0b4cf3de55d9ea395841a42c697dff3972c5b9
HST159075 <alamtasin18@gmail.com>
2026-04-02 17:17:45 +0600

    fix: add appUrl for google oauth redirect

--- dist/lib/auth.d.ts
+++ dist/lib/auth.d.ts
@@ -1,6 +1,7 @@
 export declare const auth: import("better-auth").Auth<{
     baseURL: string | undefined;
     database: (options: import("better-auth").BetterAuthOptions) => import("better-auth").DBAdapter<import("better-auth").BetterAuthOptions>;
+    appUrl: string | undefined;
     emailAndPassword: {
         enabled: true;
     };
--- dist/lib/auth.js
+++ dist/lib/auth.js
@@ -6,6 +6,7 @@ export const auth = betterAuth({
     database: prismaAdapter(prisma, {
         provider: "postgresql",
     }),
+    appUrl: process.env.CLIENT_URL,
     emailAndPassword: {
         enabled: true,
     },
